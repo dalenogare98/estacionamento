b3ef0c173b02875b4d49e2b69b30d18943804443
Pedro <dalenogare98@gmail.com>
2021-02-10 11:33:45 -0300

    testando o print

--- Estacionamento/src/Main.java
+++ Estacionamento/src/Main.java
@@ -23,6 +23,7 @@ public class Main {
         Calculador calculador = new Calculador();
         TelaMenu tela = new TelaMenu(controlador,calculador);
         tela.setVisible(true);
+        System.out.println("Iniciando o programa");
     }
     
 }
